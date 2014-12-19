4e4431b9e7385d75480aeabc44ab05a9dfe4533f
Anthony Fulginiti <anthony.fulginiti@starwoodvo.com>
2014-12-19 10:54:20 -0500

    First Commit - Version 0.1

---
+++ Fixed-Bootstrap-Site-Navigation.js
@@ -0,0 +1,14 @@
+// ==UserScript==
+// @name			Bootstrap Site Navigation Userscript
+// @homepage		https://github.com/anthonycl/Bootstrap-Site-Navigation-Userscript
+// @version			0.1
+// @description		Greasemonkey/Tampermonkey Userscript automatically fix the bootstrap site's navigation to the top as you scroll.
+// @author			Anthony Fulginiti <anthony@cliklabs.com>
+// @match			http://*.getbootstrap.com/*
+// @include			http://*.getbootstrap.com/*
+// @grant			none
+// @updateURL		https://raw.githubusercontent.com/anthonycl/Bootstrap-Site-Navigation-Userscript/master/Fixed-Bootstrap-Site-Navigation.js
+// ==/UserScript==
+
+var d = document.getElementById("top");
+d.className = d.className + " navbar-fixed-top";
